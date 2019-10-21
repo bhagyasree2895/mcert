300f31c61ef59fb9ad14bd5d1b992f3215b18af6
Bhagya Sree Chanda <bhagyasree.c60@gmail.com>
2019-10-21 00:51:36 -0500

    committing after adding general report activity and xml file for the corresponding activity.

--- mcert/app/src/main/java/com/example/mcert/GeneralReport.java
+++ mcert/app/src/main/java/com/example/mcert/GeneralReport.java
@@ -5,8 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class GeneralReport extends AppCompatActivity {
+    public static final int TASK_REQ = 1;
+    public static final int TASK_RES = 1;
+
+    public static final int GOOD_RES = 2;
+
+    public static final int TOG_RES = 2;
+
+    public static final int FLO_RES = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,4 +27,45 @@ public class GeneralReport extends AppCompatActivity {
         setResult(TabsActivity.RES,gen_ini);
         finish();
     }
+    public void onSubmit(View v) {
+        Intent ini = new Intent();
+        setResult(TabsActivity.GOOD_RES, ini);
+        finish();
+    }
+
+    public void getLocationAction(View v) {
+        Intent ini = new Intent(this, MapsActivity.class);
+        startActivity(ini);
+    }
+
+    public void getDisasterType(View v) {
+        Intent disaster_ini = new Intent(this, ImagesActivity.class);
+        startActivityForResult(disaster_ini, TASK_REQ);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent disasterInt) {
+        if (requestCode == TASK_REQ) {
+            if (resultCode == TASK_RES) {
+                String str = disasterInt.getStringExtra("name");
+                EditText edt = findViewById(R.id.editText5);
+                edt.setText(str);
+            }
+            else if (resultCode == GOOD_RES) {
+                String str = disasterInt.getStringExtra("disaster");
+                EditText edt = findViewById(R.id.editText5);
+                edt.setText(str);
+            }
+            else if(resultCode == TOG_RES){
+                String str = disasterInt.getStringExtra("disaster");
+                EditText edt = findViewById(R.id.editText5);
+                edt.setText(str);
+            }
+            else if(resultCode == FLO_RES){
+                String str = disasterInt.getStringExtra("disaster");
+                EditText edt = findViewById(R.id.editText5);
+                edt.setText(str);
+            }
+        }
+    }
 }
+
